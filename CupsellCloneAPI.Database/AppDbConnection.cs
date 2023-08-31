49be78f1af401a7f1b41fab50362d0551f90ac4f
Lukasz Gawron <51411768+LUKIS123@users.noreply.github.com>
2023-09-01 00:17:46 +0200

    db conn

---
+++ CupsellCloneAPI.Database/AppDbConnection.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace CupsellCloneAPI.Database
+{
+    public class AppDbConnection : IDbConnectionFactory
+    {
+        private readonly string _connectionString;
+
+        public AppDbConnection(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IDbConnection GetDbConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}

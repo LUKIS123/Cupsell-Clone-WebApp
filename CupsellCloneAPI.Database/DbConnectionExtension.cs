ddaba6bc4b6f2462799d259ee8197aea8951f027
Lukasz Gawron <51411768+LUKIS123@users.noreply.github.com>
2023-09-03 19:50:44 +0200

    frame 1

---
+++ CupsellCloneAPI.Database/DbConnectionExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CupsellCloneAPI.Database
+{
+    public static class DbConnectionExtension
+    {
+        public static IServiceCollection AddSqlConnectionFactory(
+            this IServiceCollection serviceCollection,
+            string connectionString
+        )
+        {
+            serviceCollection.AddScoped<IDbConnectionFactory>(provider =>
+                new SqlDbConnectionFactory(connectionString)
+            );
+            return serviceCollection;
+        }
+    }
+}
---
+++ CupsellCloneAPI.Database/Entities/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace CupsellCloneAPI.Database.Entities
+{
+    public class Role
+    {
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
